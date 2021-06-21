1c766e3249c1566317131e4c05886a9dc8e4c8bc
gautam-jha <gautamjha.mailbox@gmail.com>
2021-06-22 00:20:17 +0530

    Share buttons added

--- components/Meme.js
+++ components/Meme.js
@@ -1,3 +1,18 @@
+import {
+    FacebookShareButton,
+    FacebookIcon,
+    LineShareButton,
+    LineIcon,
+    PinterestShareButton,
+    PinterestIcon,
+    TelegramShareButton,
+    TelegramIcon,
+    TwitterShareButton,
+    TwitterIcon,
+    WhatsappShareButton,
+    WhatsappIcon
+} from 'next-share';
+
 import styles from '../styles/Meme.module.css';
 
 function Meme(props) {
@@ -23,8 +38,34 @@ function Meme(props) {
                         height="50"
                     />
                 )}
-                <div className="px-6 py-4">
+                <div className="px-6 py-4 m-1 social_icons">
                     <div className="font-bold text-sm mb-2">{data.title}</div>
+                    <FacebookShareButton
+                        url={data.url}
+                        quote={data.title}
+                        className="m-5"
+                        // hashtag={'#random-next-meme'}
+                    >
+                        <FacebookIcon size={32} round />
+                    </FacebookShareButton>
+                    <LineShareButton url={data.url} title={data.title} className="m-5">
+                        <LineIcon size={32} round />
+                    </LineShareButton>
+                    <PinterestShareButton
+                        url="https://random-next-meme.vercel.app/"
+                        media={data.url}>
+                        <PinterestIcon size={32} round />
+                    </PinterestShareButton>
+
+                    <TelegramShareButton url={data.url} title={data.title}>
+                        <TelegramIcon size={32} round />
+                    </TelegramShareButton>
+                    <TwitterShareButton url={data.url} title={data.title}>
+                        <TwitterIcon size={32} round />
+                    </TwitterShareButton>
+                    <WhatsappShareButton url={data.url} title={data.title} separator=":: ">
+                        <WhatsappIcon size={32} round />
+                    </WhatsappShareButton>
                 </div>
             </div>
         )
